84061085e1740d9f9541db2f56826812b35520bb
Saulo Oliveira <sauslash@hotmail.com>
2020-03-24 18:54:19 -0300

    initial commit

---
+++ backend/src/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const ongController = require('./controllers/OngController');
+const incidentsController = require('./controllers/IncidentController');
+const profileController = require('./controllers/ProfileController');
+const sessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', sessionController.create);
+
+routes.get('/ongs', ongController.index);
+routes.post('/ongs', ongController.create);
+
+routes.get('/profile', profileController.index);
+
+routes.get('/incidents', incidentsController.index);
+routes.post('/incidents', incidentsController.create);
+routes.delete('/incidents/:id', incidentsController.delete);
+
+module.exports = routes;
